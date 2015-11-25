d4f036d5e0faf421fcf2750995f6934c179b39c7
Larmie <larmiereuben@gmail.com>
2015-11-25 20:54:19 +0000

    no change done

--- admin.php
+++ admin.php
@@ -8,3 +8,15 @@
 			}
 		}
 		?>
+		
+		
+<?php 		
+include_once ("adb.php");
+    class Administrator extends adb{
+		function deleteAdministrator($id){
+	            $str_query = "delete from administrators
+				              where employee_id = '$id'";
+		        return $this->query($str_query);
+		    }
+	}
+			?>
