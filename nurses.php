8299a06f4bef1fccd79792af5c91f2987f6b49e2
Rahab <bakarahab93@gmail.com>
2015-11-25 03:45:16 +0000

    finished commenting on nurses.php and nursesAjax.php

--- nurses.php
+++ nurses.php
@@ -2,8 +2,14 @@
 include "adb.php";
 /*A nurse class*/
 class Nurses extends adb{
-	/*Function to add nurse*/
-	function addNurse($id,$firstname,$secondname,&contact){
+	/**
+	*@param $id id of the nurse
+	*@param $firstname, first name of the nurse
+	*@param $secondname, surname of the nurse
+	*@param $contact, nurses contact
+	*@return return a boolean value, yes or no
+	*/
+	function addNurse($id,$firstname,$secondname,$contact){
 		$str_query="INSERT INTO addnurse (nurseid,nursefname,nursesname,nursecontact)
 		            Values($id,'$firstname','$secondname','&contact')";
 		           // echo "$str_query";
--- nursesAjax.php
+++ nursesAjax.php
@@ -1,17 +1,15 @@
 <?php
-
+/*checks whether cmd was passed*/
 if(!isset($_REQUEST['cmd'])){
 	echo '{"result": 0, "message": "sijui command"}';
 	return;
 }
+/*checks the value of cmd, to assign to its relevant function*/
 $cmd = $_REQUEST['cmd'];
 
 switch ($cmd) {
 	case '1':
 		addNurse();
-		break;
-	case '2':
-		deleteNurse();
 		break;
 	
 	default:
@@ -19,15 +17,18 @@ echo '{"result": 0, "message": "Unknown command"}';
 	return;
 	break;
 }
-
+/*function to add a nurse, its called incase cmd=1*/
 function addNurse(){
 	include "nurses.php";
+	/*creats an object of the nurse class*/
 	$theNurse = new Nurses();
 	$id= $_GET['nurseid'];
 	$firstname= $_GET['nursefname'];
 	$secondname= $_GET['nursesname'];
 	$contact= $_GET['nursecontact'];
 
+
+	/*adds  a nurse to the database by calling addnurse method in the nurse class*/
 	if (!$theNurse->addNurse($id,$firstname,$secondname,$contact)) {
 
 		echo '{"result": 0, "message": "could not add nurse"}';
@@ -38,10 +39,6 @@ function addNurse(){
 	}return;
 
 }
-function deleteNurse(){
-	include "nurses.php";
-	$theNurse = new Nurses();
-	$id = $_GET['nurseid'];
-}
+
 
 ?>
