96c3b3001d32b03ae6298da42c0bde8f2b2e3467
Larmie <larmiereuben@gmail.com>
2015-11-26 00:30:08 +0000

    Commenting the deleteAdmin code and applying proper coding standards

--- deleteadmin.php
+++ deleteadmin.php
@@ -1,10 +1,19 @@
 <?php
-include_once ("adb.php");
-class Administrator extends adb{
-function deleteAdministrator($id){
-	            $str_query = "delete from administrators
-				              where employee_id = '$id'";
-		        return $this->query($str_query);
-		    }
-			}
-		?>
+/**
+*Connecting the adb function to the deleteNurse function to include functionalities
+*/
+    include_once ("adb.php");
+    class Administrator extends adb{
+/**
+*Creating a the class, delete administrators
+*/
+    function deleteAdministrator($id){
+/**
+*Code that queries the database and removes the selected administrator from the database
+*/
+	$str_query = "delete from administrators
+				  where employee_id = '$id'";
+    return $this->query($str_query);
+		}
+	}
+?>
