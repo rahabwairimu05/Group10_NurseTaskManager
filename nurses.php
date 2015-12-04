aa042875f8e7ae8e15755dc265c2b8884ff81a8b
Jonathan.Aherdemla <jaherdemla@gmail.com>
2015-12-04 02:30:08 +0000

    implemented the delete function

--- nurses.php
+++ nurses.php
@@ -6,5 +6,10 @@ class Nurses extends adb{
 		$str_query="SELECT employee_id, first_name,last_name,contact FROM nurses";
 		return $this->query($str_query);
 	}
+
+	function deleteNurse($Eid){
+            $str_query="DELETE FROM nurses WHERE employee_id =$Eid";
+            return $this->query($str_query);
+      }
 }
 ?>
